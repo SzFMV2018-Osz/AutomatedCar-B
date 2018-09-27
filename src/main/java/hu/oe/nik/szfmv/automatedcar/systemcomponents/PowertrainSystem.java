7992bb6331473d13df41a3c55cd751e8b6f15f60
Kiss Adam <kissadam996@gmail.com>
2018-09-27 15:30:24 +0200

    Car engine model, Rpm calculation

--- src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/PowertrainSystem.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/PowertrainSystem.java
@@ -1,31 +1,62 @@
 package hu.oe.nik.szfmv.automatedcar.systemcomponents;
 
 import hu.oe.nik.szfmv.automatedcar.bus.VirtualFunctionBus;
+import hu.oe.nik.szfmv.automatedcar.engine.CarEngine;
+import hu.oe.nik.szfmv.automatedcar.engine.StandardCarEngine;
 
 /**
  * Powertrain system is responsible for the movement of the car.
  */
 public class PowertrainSystem extends SystemComponent {
-    private double speed;
+
+    private final CarEngine engine;
+    private int rpm;
+    private int currentGear;
 
     /**
      * Creates a powertrain system that connects the Virtual Function Bus
      *
-     * @param virtualFunctionBus {@link VirtualFunctionBus} used to connect {@link SystemComponent}s
+     * @param virtualFunctionBus {@link VirtualFunctionBus} used to connect
+     *                           {@link SystemComponent}s
      */
-    public PowertrainSystem(VirtualFunctionBus virtualFunctionBus) {
-        super(virtualFunctionBus);
+    public PowertrainSystem(final VirtualFunctionBus virtualFunctionBus) {
+	super(virtualFunctionBus);
+	engine = new StandardCarEngine();
+	rpm = 0;
+	currentGear = 1;
+    }
+
+    public int getCurrentGear() {
+	return currentGear;
     }
 
     @Override
     public void loop() {
-        int gasPedal = virtualFunctionBus.samplePacket.getGaspedalPosition();
-        speed = gasPedal * 0.8;
-        //TODO write this
+
     }
 
-    public double getSpeed() {
-        return this.speed;
+    public int updateEngine(final double wheelRotationRate) {
+	if (wheelRotationRate >= 0) {
+	    updateEngineState(wheelRotationRate);
+	}
+	return rpm;
+    }
+
+    private void updateEngineState(final double wheelRotationRate) {
+	rpm = calculateRpm(wheelRotationRate);
+	updateGear();
+    }
+
+    private int calculateRpm(final double wheelRotationRate) {
+	return (int) ((wheelRotationRate * engine.getGearRatios()[currentGear] * engine.getGearDifferentialRatio() * 60)
+		/ (2 * Math.PI));
+    }
+
+    private void updateGear() {
+	if ((currentGear < engine.getGearCount()) && (rpm > engine.getGearShiftRpm())) {
+	    currentGear++;
+	} else if ((currentGear > 1) && (rpm < engine.getBackGearShiftRpm())) {
+	    currentGear--;
+	}
     }
 }
-
