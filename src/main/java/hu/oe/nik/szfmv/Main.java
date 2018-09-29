907339b7f929c551750361a81ceff5ab437dea55
dani <dani.rausz@gmail.com>
2018-09-29 17:29:29 +0200

    Fix FPS megoldva. Kiszámolja az eltelt ciklusidőt és a szükséges ideig altatja a szálat, hogy a kívánt frekvencián frissüljön a kép.

--- src/main/java/hu/oe/nik/szfmv/Main.java
+++ src/main/java/hu/oe/nik/szfmv/Main.java
@@ -3,13 +3,24 @@ package hu.oe.nik.szfmv;
 import hu.oe.nik.szfmv.automatedcar.AutomatedCar;
 import hu.oe.nik.szfmv.common.ConfigProvider;
 import hu.oe.nik.szfmv.environment.World;
+import hu.oe.nik.szfmv.environment.WorldObject;
 import hu.oe.nik.szfmv.visualization.Gui;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Calendar;
+
 public class Main {
     private static final Logger LOGGER = LogManager.getLogger();
-    private static final int CYCLE_PERIOD = 40;
+    private static int CYCLE_PERIOD = 40;
+
+    // Ezt az FPS-t szeretnénk tartani
+    public static final int TARGET_FPS = 24;
+
+    // Az aktuális renderelési és számítási ciklus kezdetének időpontja
+    private static long cycle_start;
+    // Az aktuális renderelési és számítási ciklus hossza
+    private static long cycle_length;
 
     /**
      * Main entrypoint of the software
@@ -34,10 +45,20 @@ public class Main {
         // draw world to course display
         gui.getCourseDisplay().drawWorld(w);
 
+        Calendar cal = Calendar.getInstance();
+
         while (true) {
             try {
+                cycle_start = cal.getTimeInMillis();
                 car.drive();
                 gui.getCourseDisplay().drawWorld(w);
+
+                // FIX FPS
+                cycle_length = cal.getTimeInMillis() - cycle_start;
+
+                // Szükséges késleltetési idő kiszámítása (eltelt idő * TARGET FPS)
+                CYCLE_PERIOD = (int)(1000 - (cycle_length * TARGET_FPS)) / TARGET_FPS;
+                System.out.println("FPS/TARGET FPS: " + (1000/CYCLE_PERIOD) + "/" + TARGET_FPS);
                 Thread.sleep(CYCLE_PERIOD);
             } catch (InterruptedException e) {
                 LOGGER.error(e.getMessage());
