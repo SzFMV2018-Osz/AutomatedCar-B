8eb0a57b3dab9f5910d79bd0df0cbecfbb7fe175
Barna <ba960919@gmail.com>
2018-10-04 19:27:17 +0200

    add transmissionmodes enum

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/engine/TransmissionModes.java
@@ -0,0 +1,12 @@
+package hu.oe.nik.szfmv.automatedcar.engine;
+
+public enum TransmissionModes {
+  Park(false),Reverse(true),Drive(true),Neutral(false);
+
+  private final boolean canItMove;
+  TransmissionModes(final boolean canItMove) { this.canItMove = canItMove; }
+  public boolean getCanItMove() { return this.canItMove; }
+}
+
+
+
