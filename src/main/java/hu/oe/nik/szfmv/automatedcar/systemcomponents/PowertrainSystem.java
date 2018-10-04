31fd051c6c9bc7774900933d9b6e3fd571fe45b6
Barna <ba960919@gmail.com>
2018-10-04 19:32:21 +0200

    Add condition to the updateEngine

--- src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/PowertrainSystem.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/PowertrainSystem.java
@@ -1,19 +1,18 @@
 package hu.oe.nik.szfmv.automatedcar.systemcomponents;
 
 import hu.oe.nik.szfmv.automatedcar.bus.VirtualFunctionBus;
-import hu.oe.nik.szfmv.automatedcar.engine.CarEngine;
-import hu.oe.nik.szfmv.automatedcar.engine.CarEngineType;
-import hu.oe.nik.szfmv.automatedcar.engine.GearBox;
-import hu.oe.nik.szfmv.automatedcar.engine.StandardCarEngineType;
+import hu.oe.nik.szfmv.automatedcar.engine.*;
 
 /**
  * Powertrain system is responsible for the movement of the car.
  */
 public class PowertrainSystem extends SystemComponent {
 
+
     private final CarEngineType engineType;
     private CarEngine engine;
     private GearBox gearBox;
+
 
     /**
      * Creates a powertrain system that connects the Virtual Function Bus
@@ -22,18 +21,23 @@ public class PowertrainSystem extends SystemComponent {
      *                           {@link SystemComponent}s
      */
     public PowertrainSystem(final VirtualFunctionBus virtualFunctionBus) {
-	super(virtualFunctionBus);
-	engineType = new StandardCarEngineType();
-	engine = new CarEngine(engineType);
-	gearBox = new GearBox(engineType);
+        super(virtualFunctionBus);
+        engineType = new StandardCarEngineType();
+        engine = new CarEngine(engineType);
+        gearBox = new GearBox(engineType);
     }
 
     public int getRpm() {
-	return engine.getRpm();
+        return engine.getRpm();
     }
 
     public int getCurrentGear() {
-	return gearBox.getCurrentGear();
+        return gearBox.getCurrentGear();
+    }
+
+    public TransmissionModes getCurrentAutomaticTransmissionModes()
+    {
+        return gearBox.getTransmissionModes();
     }
 
     @Override
@@ -42,9 +46,10 @@ public class PowertrainSystem extends SystemComponent {
     }
 
     public void updateEngine(final double wheelRotationRate) {
-	if (wheelRotationRate >= 0) {
+	if (wheelRotationRate >= 0 && gearBox.getTransmissionModes().getCanItMove()) {
 	    engine.updateRpm(wheelRotationRate, gearBox.getCurrentGear());
 	    gearBox.updateGear(engine.getRpm());
 	}
+
     }
 }
