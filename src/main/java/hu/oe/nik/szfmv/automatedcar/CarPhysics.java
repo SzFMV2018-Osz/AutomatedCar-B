0459de789d00363ec78a6b8f77dbdf06bd961ee7
valkarion <valkarion@gmail.com>
2018-10-05 12:25:45 +0200

    Added a physics class and constant air resistance calculator function

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/CarPhysics.java
@@ -0,0 +1,28 @@
+package hu.oe.nik.szfmv.automatedcar;
+
+/**
+ * Contains functions and constants to calculate
+ * physical forces
+ */
+public class CarPhysics {
+    private static double aeroDrag = 0.05;
+
+    /**
+     * @param vx the x component of the velocity vector
+     * @param vy the y component of the velocity vector
+     * @return air resistance array of component values
+     */
+    public static double[] CalcAirResistanceVector(double vx, double vy ){
+        double[] aDrag = new double[2];
+
+        if(vx != 0.0 || vy != 0.0)
+        {
+            double vLen = Math.sqrt(vx * vx + vy * vy);
+
+            aDrag[0] = vx * vLen * -aeroDrag;
+            aDrag[1] = vy * vLen * -aeroDrag;
+        }
+
+        return aDrag;
+    }
+}
