2bdee379629697e117a8761b59a26411d946fa56
Barna <ba960919@gmail.com>
2018-10-06 14:52:02 +0200

    add car axis params

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/engine/CarAxisParams.java
@@ -0,0 +1,12 @@
+package hu.oe.nik.szfmv.automatedcar.engine;
+
+public interface CarAxisParams {
+
+    int getCarWidthPixel() ;
+
+   int getAxisLengthPixel();
+
+   int getMaxTurningPercent();
+
+
+}
---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/engine/CarAxisParamsImpl.java
@@ -0,0 +1,25 @@
+package hu.oe.nik.szfmv.automatedcar.engine;
+
+public class CarAxisParamsImpl implements CarAxisParams {
+
+    final int carWidthPixel=90;
+    final int axisLengthPixel=130;
+    final int maxTurningPercent=60;
+
+    @Override
+    public int getCarWidthPixel() {
+        return carWidthPixel;
+    }
+
+    @Override
+    public int getAxisLengthPixel() {
+        return axisLengthPixel;
+    }
+
+    @Override
+    public int getMaxTurningPercent() {
+        return maxTurningPercent;
+    }
+
+
+}
