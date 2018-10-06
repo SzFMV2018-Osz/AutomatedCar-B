219c76879fd1aec9822d3929d13a3f7cf6d73841
Barna <ba960919@gmail.com>
2018-10-06 14:55:30 +0200

    update angular speed

--- src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/SteeringSystem.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/SteeringSystem.java
@@ -1,12 +1,15 @@
 package hu.oe.nik.szfmv.automatedcar.systemcomponents;
 
 import hu.oe.nik.szfmv.automatedcar.bus.VirtualFunctionBus;
+import hu.oe.nik.szfmv.automatedcar.engine.TurningHandler;
 
 /**
  * Steering system is responsible for the turning of the car.
  */
 public class SteeringSystem extends SystemComponent {
     private double angularSpeed = 0;
+    private TurningHandler turningHandler;
+
 
     /**
      * Creates a steering system that connects the Virtual Function Bus
@@ -15,11 +18,17 @@ public class SteeringSystem extends SystemComponent {
      */
     public SteeringSystem(VirtualFunctionBus virtualFunctionBus) {
         super(virtualFunctionBus);
+        turningHandler=new TurningHandler();
     }
 
     @Override
     public void loop() {
-        // TODO
+    //TODO
+    }
+
+    public void updateAngularSpeed(final int steeringWheelState,final int speed)
+    {
+           angularSpeed = turningHandler.angularVelocityCalculation(steeringWheelState,speed);
     }
 
     public double getAngularSpeed() {
