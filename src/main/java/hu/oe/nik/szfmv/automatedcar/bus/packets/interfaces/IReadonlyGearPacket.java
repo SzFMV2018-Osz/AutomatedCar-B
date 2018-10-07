2ec417bdd470bc8d0f4b51bb7756130f182aebe2
Miki <nagymiki08@gmail.com>
2018-10-07 22:09:23 +0200

    remove redundant Gear enum

--- src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/interfaces/IReadonlyGearPacket.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/interfaces/IReadonlyGearPacket.java
@@ -1,6 +1,6 @@
 package hu.oe.nik.szfmv.automatedcar.bus.packets.interfaces;
 
-import hu.oe.nik.szfmv.common.enums.Gear;
+import hu.oe.nik.szfmv.automatedcar.bus.userinput.enums.Gear;
 
 public interface IReadonlyGearPacket {
     Gear getCurrentGear();
--- src/main/java/hu/oe/nik/szfmv/common/enums/Gear.java
+++
@@ -1,5 +0,0 @@
-package hu.oe.nik.szfmv.common.enums;
-
-public enum Gear {
-    P, R, N, D
-}
