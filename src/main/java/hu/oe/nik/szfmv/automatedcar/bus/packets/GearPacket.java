1353f9a99d0595a1f309865d6da37e1b436405c2
Miki <nagymiki08@gmail.com>
2018-10-07 22:40:41 +0200

    implemented gear packet

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/GearPacket.java
@@ -0,0 +1,32 @@
+package hu.oe.nik.szfmv.automatedcar.bus.packets;
+
+import hu.oe.nik.szfmv.automatedcar.bus.packets.interfaces.IReadonlyGearPacket;
+import hu.oe.nik.szfmv.automatedcar.bus.userinput.IUserInput;
+import hu.oe.nik.szfmv.automatedcar.bus.userinput.enums.Gear;
+import hu.oe.nik.szfmv.automatedcar.bus.userinput.eventhandlers.IShiftingEventHandler;
+
+/*
+ * Controller class for the gear state.
+ */
+public class GearPacket implements IReadonlyGearPacket, IShiftingEventHandler {
+
+    private final IUserInput input;
+
+    private Gear currentGear;
+
+    public GearPacket(IUserInput input) {
+        this.input = input;
+        this.input.subscribeShiftingEvents(this);
+        this.currentGear = Gear.N;
+    }
+
+    @Override
+    public Gear getCurrentGear() {
+        return this.currentGear;
+    }
+
+    @Override
+    public void onShifting(Gear gear) {
+        this.currentGear = gear;
+    }
+}
--- src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/InputManager.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/InputManager.java
@@ -1,6 +1,7 @@
 package hu.oe.nik.szfmv.automatedcar.systemcomponents;
 
 import hu.oe.nik.szfmv.automatedcar.bus.VirtualFunctionBus;
+import hu.oe.nik.szfmv.automatedcar.bus.packets.GearPacket;
 import hu.oe.nik.szfmv.automatedcar.bus.packets.IndicationPacket;
 import hu.oe.nik.szfmv.automatedcar.bus.userinput.IUserInput;
 import hu.oe.nik.szfmv.automatedcar.bus.userinput.UserInputProvider;
@@ -14,11 +15,14 @@ public class InputManager extends SystemComponent {
     private final IUserInput userInput = UserInputProvider.getUserInput(InputType.Keyboard);
 
     private final IndicationPacket indicationPacket;
+    private final GearPacket gearPacket;
 
     public InputManager(VirtualFunctionBus virtualFunctionBus) {
         super(virtualFunctionBus);
         this.indicationPacket = new IndicationPacket(userInput);
+        this.gearPacket = new GearPacket(userInput);
         virtualFunctionBus.indicationPacket = this.indicationPacket;
+        virtualFunctionBus.gearPacket = this.gearPacket;
     }
 
     @Override
