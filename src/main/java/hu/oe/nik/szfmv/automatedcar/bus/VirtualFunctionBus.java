17fd725f7754ce8fe2f6910cc1ff8f19408f9eae
Miki <nagymiki08@gmail.com>
2018-10-09 16:36:51 +0200

    connect pedal packet to virtual function bus

--- src/main/java/hu/oe/nik/szfmv/automatedcar/bus/VirtualFunctionBus.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/bus/VirtualFunctionBus.java
@@ -2,6 +2,7 @@ package hu.oe.nik.szfmv.automatedcar.bus;
 
 import hu.oe.nik.szfmv.automatedcar.bus.packets.interfaces.IReadonlyGearPacket;
 import hu.oe.nik.szfmv.automatedcar.bus.packets.interfaces.IReadonlyIndicationPacket;
+import hu.oe.nik.szfmv.automatedcar.bus.packets.interfaces.IReadonlyPedalPacket;
 import hu.oe.nik.szfmv.automatedcar.bus.packets.sample.ReadOnlySamplePacket;
 import hu.oe.nik.szfmv.automatedcar.systemcomponents.SystemComponent;
 
@@ -19,6 +20,7 @@ public class VirtualFunctionBus {
     public ReadOnlySamplePacket samplePacket;
     public IReadonlyIndicationPacket indicationPacket;
     public IReadonlyGearPacket gearPacket;
+    public IReadonlyPedalPacket pedalPacket;
 
     private List<SystemComponent> components = new ArrayList<>();
 
--- src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/InputManager.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/InputManager.java
@@ -3,6 +3,7 @@ package hu.oe.nik.szfmv.automatedcar.systemcomponents;
 import hu.oe.nik.szfmv.automatedcar.bus.VirtualFunctionBus;
 import hu.oe.nik.szfmv.automatedcar.bus.packets.GearPacket;
 import hu.oe.nik.szfmv.automatedcar.bus.packets.IndicationPacket;
+import hu.oe.nik.szfmv.automatedcar.bus.packets.PedalPacket;
 import hu.oe.nik.szfmv.automatedcar.bus.userinput.IUserInput;
 import hu.oe.nik.szfmv.automatedcar.bus.userinput.UserInputProvider;
 import hu.oe.nik.szfmv.automatedcar.bus.userinput.enums.InputType;
@@ -16,18 +17,22 @@ public class InputManager extends SystemComponent {
 
     private final IndicationPacket indicationPacket;
     private final GearPacket gearPacket;
+    private final PedalPacket pedalPacket;
 
     public InputManager(VirtualFunctionBus virtualFunctionBus) {
         super(virtualFunctionBus);
         this.indicationPacket = new IndicationPacket(userInput);
         this.gearPacket = new GearPacket(userInput);
+        this.pedalPacket = new PedalPacket(userInput);
         virtualFunctionBus.indicationPacket = this.indicationPacket;
         virtualFunctionBus.gearPacket = this.gearPacket;
+        virtualFunctionBus.pedalPacket = this.pedalPacket;
     }
 
     @Override
     public void loop() {
         this.indicationPacket.createSnapshot();
         this.gearPacket.createSnapshot();
+        this.pedalPacket.createSnapshot();
     }
 }
