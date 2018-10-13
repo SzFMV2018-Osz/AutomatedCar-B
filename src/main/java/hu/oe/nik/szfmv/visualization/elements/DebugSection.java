185b2f82c3fec7e672e3e1666f8320561b1a6187
MML\ibafa <istvan.boros93@gmail.com>
2018-10-13 11:16:12 +0200

    created and implemented the JPanel module for the debug section display

---
+++ src/main/java/hu/oe/nik/szfmv/visualization/elements/DebugSection.java
@@ -0,0 +1,70 @@
+package hu.oe.nik.szfmv.visualization.elements;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DebugSection extends JPanel {
+    private final int sectionPositionX = 10;
+    private final int sectionPositionY = 610;
+
+    private final int sectionRowSize = 15;
+
+    private final int mainDebugOffset = 0;
+    private final int steeringDebugOffset = 1;
+    private final int positionDebugOffset = 2;
+
+    public int getMainDebugOffset() {
+        return sectionRowSize * mainDebugOffset;
+    }
+    public int getSteeringDebugOffset() {
+        return sectionRowSize * steeringDebugOffset;
+    }
+    public int getPositionDebugOffset() {
+        return sectionRowSize * positionDebugOffset;
+    }
+
+    public String getMainDebugText() {
+        return "debug:";
+    }
+    public String getSteeringDebugText() {
+        return "steering wheel: ";
+    }
+    public String getPositionTextX() {
+        return "x: ";
+    }
+    public String getPositionTextY() {
+        return ", y: ";
+    }
+
+    /**
+     * Creates and returns a JLabel with the specified parameters
+     *
+     * @param text - text for the label
+     * @param rowOffset - determines the y coordinate for the label
+     * @return - returns the created JLabel
+     */
+    public JLabel getDebugLabel(String text, int rowOffset) {
+        JLabel debugLabel = new JLabel(text);
+
+        setupLabel(debugLabel, rowOffset);
+
+        return debugLabel;
+    }
+
+    /**
+     * Actualises the bounds and text color for the given JLabel
+     *
+     * @param label - the modifiable JLabel's reference
+     * @param rowOffset - determines the y coordinate for the label
+     */
+    public void setupLabel(JLabel label, int rowOffset) {
+        Insets insets = getInsets();
+        Dimension labelSize = label.getPreferredSize();
+
+        int boundsX = sectionPositionX + insets.left;
+        int boundsY = sectionPositionY + insets.top + rowOffset;
+
+        label.setBounds(boundsX, boundsY, labelSize.width, labelSize.height);
+        label.setForeground(Color.black);
+    }
+}
