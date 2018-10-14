24d0807b7f2fe75d22c8474bb2ec42632257dab6
Barna <ba960919@gmail.com>
2018-10-14 13:54:34 +0200

    calculation new speed

--- src/main/java/hu/oe/nik/szfmv/automatedcar/engine/StandardCarEngineType.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/engine/StandardCarEngineType.java
@@ -6,13 +6,18 @@ public class StandardCarEngineType implements CarEngineType {
     private final int maxRpm = 6000;
     private final int gearShiftRpm = 3000;
     private final double gearDifferentialRatio = 3;
-    private final double[] gearRatios = new double[] { 2.9, 2.6, 1.7, 1.3, 1, 0.7, 0.5 };
-    private final int[] torqueCurve = new int[] { 400, 440, 460, 480, 470, 400 };
+    private final double[] gearRatios = new double[]{2.9, 2.6, 1.7, 1.3, 1, 0.7, 0.5};
+    private final int[] torqueCurve = new int[]{400, 440, 460, 480, 470, 400};
     private final int torqueCurveStepSize = 1000;
     private final int backGearShiftRpm = 1500;
+    private final double wheelRadius = 0.33;
     private double transmissionEffiency = 0.7;
 
     @Override
+    public double getWheelRadius() {
+        return wheelRadius;
+    }
+
     public int getGearCount() {
         return gearCount;
     }
