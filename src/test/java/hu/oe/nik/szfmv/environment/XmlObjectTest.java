bbf4e35a84c9d7852f8606479bff3ac13b53db5a
porkolabgergo <41578426+porkolabgergo@users.noreply.github.com>
2018-10-17 11:21:53 +0200

    Update XmlObjectTest.java
    Hibás sor törlése.

--- src/test/java/hu/oe/nik/szfmv/environment/XmlObjectTest.java
+++ src/test/java/hu/oe/nik/szfmv/environment/XmlObjectTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class XmlObjectTest {
-    XmlObject testObject = new XmlObject(3,3,"tree",)
 
     @Test
     public void getX() {
