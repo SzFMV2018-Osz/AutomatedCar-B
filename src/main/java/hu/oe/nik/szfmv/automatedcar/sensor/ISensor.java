320785b19dc84362e93f98cddeaa59329cde3203
Dezső Roland Póka <pdezsi96@gmail.com>
2018-10-18 23:24:44 +0200

    Replace sensor interface, fix letter case issue

--- src/main/java/hu/oe/nik/szfmv/automatedcar/sensor/ISensor.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/sensor/ISensor.java
@@ -1,4 +1,4 @@
-package hu.oe.nik.szfmv.automatedcar.bus.packets.interfaces;
+package hu.oe.nik.szfmv.automatedcar.sensor;
 
 import hu.oe.nik.szfmv.environment.WorldObject;
 
@@ -16,7 +16,7 @@ public interface ISensor {
      * @param x3 third coordinate of the triangle area
      * @return the collidable world objects found in the specified area
      */
-    List<WorldObject> GetCollidableWorldObjectsFromArea(int x1, int x2, int x3);
+    List<WorldObject> getCollidableWorldObjectsFromArea(int x1, int x2, int x3);
 
     /**
      * Method for providing  non-collidable world objects from a specific area to the sensors
@@ -25,5 +25,5 @@ public interface ISensor {
      * @param x3 third coordinate of the triangle area
      * @return the non-collidable world objects found in the specified area
      */
-    List<WorldObject> GetNonCollidableWorldObjectsFromArea(int x1, int x2, int x3);
+    List<WorldObject> getNonCollidableWorldObjectsFromArea(int x1, int x2, int x3);
 }
