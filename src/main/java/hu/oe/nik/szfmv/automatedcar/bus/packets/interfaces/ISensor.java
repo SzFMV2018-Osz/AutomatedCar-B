5be631ca4a653528678e23b7d65d77ba6b468e9f
daneelolivaw <daneel92@gmail.com>
2018-10-18 20:16:27 +0200

    Add interface for collecting collidable and non-collidable world objects in a given triangle area

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/interfaces/ISensor.java
@@ -0,0 +1,29 @@
+package hu.oe.nik.szfmv.automatedcar.bus.packets.interfaces;
+
+import hu.oe.nik.szfmv.environment.WorldObject;
+
+import java.util.List;
+
+/**
+ * Provides world objects needed for the sensors.
+ */
+public interface ISensor {
+
+    /**
+     * Method for providing collidable world objects from a specific area to the sensors
+     * @param x1 first coordinate of the triangle area
+     * @param x2 second coordinate of the triangle area
+     * @param x3 third coordinate of the triangle area
+     * @return the collidable world objects found in the specified area
+     */
+    List<WorldObject> GetCollidableWorldObjectsFromArea(int x1, int x2, int x3);
+
+    /**
+     * Method for providing  non-collidable world objects from a specific area to the sensors
+     * @param x1 first coordinate of the triangle area
+     * @param x2 second coordinate of the triangle area
+     * @param x3 third coordinate of the triangle area
+     * @return the non-collidable world objects found in the specified area
+     */
+    List<WorldObject> GetNonCollidableWorldObjectsFromArea(int x1, int x2, int x3);
+}
