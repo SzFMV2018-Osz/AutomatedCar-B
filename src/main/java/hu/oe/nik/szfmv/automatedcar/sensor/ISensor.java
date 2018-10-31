c124de188971b5ada92e27a79dc9afcb98bd7710
daneelolivaw <daneel92@gmail.com>
2018-10-31 20:36:35 +0100

    Revert "sensor base class + ultrasound sensor"
    This reverts commit 2aea518

--- src/main/java/hu/oe/nik/szfmv/automatedcar/sensor/ISensor.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/sensor/ISensor.java
@@ -2,7 +2,6 @@ package hu.oe.nik.szfmv.automatedcar.sensor;
 
 import hu.oe.nik.szfmv.environment.WorldObject;
 
-import java.awt.*;
 import java.util.List;
 
 /**
@@ -17,7 +16,7 @@ public interface ISensor {
      * @param x3 third coordinate of the triangle area
      * @return the collidable world objects found in the specified area
      */
-    List<WorldObject> getCollidableWorldObjectsFromArea(Point x1, Point x2, Point x3);
+    List<WorldObject> getCollidableWorldObjectsFromArea(int x1, int x2, int x3);
 
     /**
      * Method for providing  non-collidable world objects from a specific area to the sensors
@@ -26,5 +25,5 @@ public interface ISensor {
      * @param x3 third coordinate of the triangle area
      * @return the non-collidable world objects found in the specified area
      */
-    List<WorldObject> getNonCollidableWorldObjectsFromArea(Point x1, Point x2, Point x3);
+    List<WorldObject> getNonCollidableWorldObjectsFromArea(int x1, int x2, int x3);
 }
