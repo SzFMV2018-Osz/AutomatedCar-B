2cbeabc9d01143b923095ac98a5f1dfad0fd3c3c
Kovács István <kovacs.istone23@hotmail.com>
2018-11-02 13:08:35 +0100

    Create IRadar interface implement getter and setter methods: * Number of lanes * right side closes lane number * left side closes lane number * two closes lane * actual lane

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/RadarPacket.java
@@ -0,0 +1,4 @@
+package hu.oe.nik.szfmv.automatedcar.bus.packets;
+
+public class RadarPacket {
+}
---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/interfaces/IRadarPacket.java
@@ -0,0 +1,53 @@
+package hu.oe.nik.szfmv.automatedcar.bus.packets.interfaces;
+
+public interface IRadarPacket {
+    /**
+     * @return number of lanes
+     */
+    int getLaneNumber();
+
+    /**
+     * @param laneNumber Set number of road lanes
+     */
+    void setLaneNumber(int laneNumber);
+
+    /**
+     * @return Right closes Lane
+     */
+    int getRightClosesRoadLane();
+
+    /**
+     * @param laneNumber Set Right closes lane number
+     */
+    void setRightClosesRoadLane(int laneNumber);
+
+    /**
+     * @return Left closes Lane
+     */
+    int getLeftClosesRoadLane();
+
+    /**
+     * @param laneNumber Set Right closes lane number
+     */
+    void setLeftClosesRoadLane(int laneNumber);
+
+    /**
+     * @return Actual Road Lane number
+     */
+    int getActualtRoadLane();
+
+    /**
+     * @param laneNumber Set Actual Lane number
+     */
+    void setActualtRoadLane(int laneNumber);
+
+    /**
+     * @return get two closer lanes
+     */
+    int[] getTwoCloserLane();
+
+    /**
+     * @param actualRoadLane Set Two closer lane number
+     */
+    void setTwoCloserLane(int actualRoadLane);
+}
