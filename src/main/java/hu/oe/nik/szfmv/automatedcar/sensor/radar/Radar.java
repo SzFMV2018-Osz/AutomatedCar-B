039dcf6473e58bedf17eb4b1bd8cc09cc1542ca6
Barna <ba960919@gmail.com>
2018-11-02 15:37:38 +0100

    fix name

--- src/main/java/hu/oe/nik/szfmv/automatedcar/sensor/radar/Radar.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/sensor/radar/Radar.java
@@ -16,7 +16,7 @@ public class Radar {
      * @param worldObjects list of world objects
      * @return These are in triangle
      */
-    public List<WorldObject> finWorldObjectsInRadarTriangle(List<WorldObject> worldObjects) {
+    public List<WorldObject> findWorldObjectsInRadarTriangle(List<WorldObject> worldObjects) {
         List<WorldObject> inTriangleList = new ArrayList<>();
         for (WorldObject wordObject : worldObjects) {
             if (triangle.isInTheTriangle(wordObject.getX(), wordObject.getY())) {
