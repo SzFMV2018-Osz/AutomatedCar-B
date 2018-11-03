fbf3de299d2269f04e8554ae5a3de74d16903e14
kovacsistvan1 <istvan@sysroot.hu>
2018-11-03 09:32:14 +0100

    Refactor interface IRadarPacket. Fix the javadoc return parameter.

--- src/test/java/hu/oe/nik/szfmv/automatedcar/bus/packets/interfaces/IRadarPacketTest.java
+++ src/test/java/hu/oe/nik/szfmv/automatedcar/bus/packets/interfaces/IRadarPacketTest.java
@@ -4,31 +4,33 @@ import hu.oe.nik.szfmv.environment.worldobjectclasses.Collidable;
 import junitparams.Parameters;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class IRadarPacketTest {
+    @Ignore("Ignore")
     @Test
     public int getLaneNumber() {
         return 0;
     }
-
+    @Ignore("Ignore")
     @Test
     @Parameters({"111","1234"})
     public void setLaneNumber(int laneNumber) {
     }
-
+    @Ignore("Ignore")
     @Test
     public Collidable getRightClosesObject() {
         return null;
     }
-
+    @Ignore("Ignore")
     @Test
     public Collidable getLeftClosesObject() {
         return null;
     }
-
+    @Ignore("Ignore")
     @Test
     public Collidable getActualLaneClosesObjects() {
         return null;
