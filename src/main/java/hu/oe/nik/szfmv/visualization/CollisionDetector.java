b7151d810810a71b54f7eb9c389d4521b79059a2
dani <dani.rausz@gmail.com>
2018-11-03 21:19:01 +0100

    #180 Singleton CollisionDetector,  findObstacles metódus

--- src/main/java/hu/oe/nik/szfmv/visualization/CollisionDetector.java
+++ src/main/java/hu/oe/nik/szfmv/visualization/CollisionDetector.java
@@ -1,4 +1,40 @@
 package hu.oe.nik.szfmv.visualization;
 
+import hu.oe.nik.szfmv.environment.WorldObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollisionDetector {
+
+    private static CollisionDetector instance = null;
+    protected CollisionDetector() {
+        // Exists only to defeat instantiation.
+    }
+    public static CollisionDetector getInstance() {
+        if(instance == null) {
+            instance = new CollisionDetector();
+        }
+        return instance;
+    }
+
+    private static List<WorldObject> obstacles = new ArrayList<>();
+
+    /***
+     * Az ütközhető objektumok megkeresése, listába gyűjtése
+     */
+    public static void findObstacles(List<WorldObject> list){
+        // TODO: ehhez még kell az ütközős XML lista beolvasása
+        List<String> obstacleNames = new ArrayList<String>();
+
+        for (WorldObject object: list) {
+            if (  obstacleNames.contains(object.getImageFileName()) ) {
+                obstacles.add(object);
+            }
+        }
+    }
+
+    public static List<WorldObject> getObstacles() {
+        return obstacles;
+    }
 }
