5e5352c4d5f18550e57079df27ae9fdf4dab21c6
Kis Levente <kislevi94@gmail.com>
2018-11-07 17:50:04 +0100

    ColliderModell Class StyleCode and review

--- src/main/java/hu/oe/nik/szfmv/visualization/ColliderModel.java
+++ src/main/java/hu/oe/nik/szfmv/visualization/ColliderModel.java
@@ -4,51 +4,79 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
 import java.awt.geom.RectangularShape;
 
+/**
+ * Class for represent the inner spaces for the images to handle the collisions.
+ */
 public class ColliderModel {
+    /**
+     * Private String, contains the name of the image.
+     */
     private String name;
+    /**
+     * Private int, the x coordinate of the image.
+     */
     private int x;
+    /**
+     * Private int, the y coordinate of the image.
+     */
     private int y;
-    private int hight;
-    private int width;
+    /**
+     * Private RectangularShape, the shape of the image.
+     */
     private RectangularShape shape;
 
-    public ColliderModel(String name, int x, int y, int width, int hight, String shape) {
+    /**
+     * Constructor for the class.
+     * @param name      Name of the image.
+     * @param x         X coordinate of the image.
+     * @param y         Y coordinate of the image.
+     * @param width     Width of the image.
+     * @param height     Name of the image.
+     * @param shape     Shape of the image.
+     */
+    public ColliderModel(String name, int x, int y, int width, int height, String shape) {
         this.name = name;
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.hight = hight;
         if(shape.equals("rect"))
         {
-            this.shape = new Rectangle2D.Double(x,y,width,hight);
+            this.shape = new Rectangle2D.Double(0,0,width,height);
         }
         else if(shape.equals("ellipse"))
         {
-            this.shape = new Ellipse2D.Double(x,y,width,hight);
+            this.shape = new Ellipse2D.Double(0,0,width,height);
         }
     }
 
+    /**
+     * Get the shape of the image.
+     * @return Shape of the image.
+     */
     public RectangularShape getShape() {
         return shape;
     }
 
+    /**
+     * Get the name of the image.
+     * @return Name of the image.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Get the x coordinate of the image.
+     * @return X coordinate of the image.
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Get the y coordinate of the image.
+     * @return Y coordinate of the image.
+     */
     public int getY() {
         return y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHight() {
-        return hight;
     }
 }
